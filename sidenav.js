d1fdb2f9933decd03cf23146305dbe1583a4da01
MiniAddman <adamreed175@yahoo.co.uk>
2020-11-25 17:05:57 +0000

    Amended heading with navbar

--- sidenav.js
+++ sidenav.js
@@ -1,3 +1,17 @@
+function addListeners() {
+    var img = document.getElementById("navImg");
+    img.addEventListener("mouseover", navMouseover);
+    img.addEventListener("mouseout", navMouseout);
+}
+
+function navMouseover() {
+    document.getElementById("navImg").setAttribute("src", "images/compassIconEditYellow.png")
+}
+
+function navMouseout() {
+    document.getElementById("navImg").setAttribute("src", "images/compassIconEditWhite.png")
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
